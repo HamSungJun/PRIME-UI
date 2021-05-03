8174d22d4b450e74c08177885573820d3cba35b4
함성준 <jun@hamseongjun-ui-Macmini.local>
2021-05-03 22:52:29 +0900

    pui-modal-container v-for 순회 로직 데이터 매핑 이슈 수정

--- src/packages/prototypes/$modal/pui-modal-container.vue
+++ src/packages/prototypes/$modal/pui-modal-container.vue
@@ -4,11 +4,11 @@
       v-show="modalRefs.length > 0"
     >
         <pui-modal
-            v-for="modalRef in modalRefs"
-            :key="modalRef.id"
-            v-bind="modalRef"
-            @close="onClose"
-            @close-all="onCloseAll"
+          v-for="modalRef in modalRefs"
+          :key="modalRef.modalId"
+          v-bind="modalRef"
+          @close="onClose"
+          @close-all="onCloseAll"
         >
         </pui-modal>
     </div>
@@ -38,7 +38,7 @@ export default {
     },
     onClose (modalId) {
       this.modalRefs.splice(
-        this.modalRefs.findIndex(modalRef => modalRef.id === modalId),
+        this.modalRefs.findIndex(modalRef => modalRef.modalId === modalId),
         1
       )
     },
